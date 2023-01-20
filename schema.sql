b4986b0301cd50373859d2cb5c103b2480e172dd
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-01-20 12:54:12 +0630

    Add queries and answer the questions

--- schema.sql
+++ schema.sql
@@ -32,3 +32,27 @@ ADD COLUMN species_id  INT references species(id);
 
 ALTER TABLE animals
 ADD COLUMN owner_id INT references owners(id);
+
+create table vets(
+  id bigint generated always as identity primary key,
+  name varchar(100),
+  age integer,
+  date_of_graduation DATE
+);
+
+create table specializations(
+  species_id integer NOT NULL,
+  vets_id integer NOT NULL,
+  primary key(species_id, vets_id),
+  constraint species_id foreign key(species_id) references species(id),
+  constraint vets_id foreign key(species_id) references vets(id)
+);
+
+create table visits(
+  animals_id integer NOT NULL,
+  vets_id integer NOT NULL,
+  date_of_visit DATE,
+  primary key(animals_id, vets_id, date_of_visit),
+  constraint animals_id foreign key(animals_id) references animals(id),
+  constraint vets_id foreign key(vets_id) references vets(id)
+);
